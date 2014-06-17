89147f65478b553116bebe0ba3a8e841c4e90a54
Arne Burmeister <arne@burmeister-teltow.de>
2014-06-17 23:28:22 +0200

    conversion setup

---
+++ src/main/java/com/tui/fly/domain/ConnectionToStringConverter.java
@@ -0,0 +1,7 @@
+package com.tui.fly.domain;
+
+/**
+ * Created by arne on 17.06.14.
+ */
+public class ConnectionToStringConverter {
+}
---
+++ src/test/java/com/tui/fly/domain/ConnectionToStringConverterTest.java
@@ -0,0 +1,7 @@
+package com.tui.fly.domain;
+
+import static org.junit.Assert.*;
+
+public class ConnectionToStringConverterTest {
+
+}
