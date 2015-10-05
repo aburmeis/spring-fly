baec704c893595a2bb2c5e2da38b3d44fe969a33
Arne Burmeister <arne.burmeister@tui.com>
2015-10-05 10:42:56 +0200

    simplified, use boot default config way

--- src/main/java/com/tui/fly/Application.java
+++ src/main/java/com/tui/fly/Application.java
@@ -3,6 +3,7 @@ package com.tui.fly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.StandardEnvironment;
 
 import java.util.HashSet;
@@ -14,27 +15,14 @@ import static org.springframework.util.StringUtils.collectionToDelimitedString;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-    static final Set<String> VALID_PROFILES = new HashSet<>(asList("memory", "relational"));
-
     public static void main(String... args) {
         new Application().riseAndRuleWithSpringBoot(args);
     }
 
     protected void riseAndRuleWithSpringBoot(final String... args)
     {
-        String profile = "memory";
-        for (String arg : args) {
-            if (VALID_PROFILES.contains(arg)) {
-                profile = arg;
-            } else {
-                System.err.println("Usage: " + collectionToDelimitedString(VALID_PROFILES, "|", "[", "]"));
-            }
-        }
-        final SpringApplication app = new SpringApplication(Application.class);
-        StandardEnvironment env = new StandardEnvironment();
-        env.setActiveProfiles(profile);
-        app.setEnvironment(env);
-        app.run();
-        logger.info("Started using profile " + profile);
+        SpringApplication app = new SpringApplication(Application.class);
+        ConfigurableApplicationContext context = app.run(args);
+        logger.info("Started using profiles " + asList(context.getEnvironment().getActiveProfiles()));
     }
 }
