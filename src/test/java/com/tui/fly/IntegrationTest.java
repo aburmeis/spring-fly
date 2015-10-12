dfc1daba2a5121b3343fee2d92bbfcea48192c6b
Arne Burmeister <arne.burmeister@tui.com>
2015-10-12 13:22:25 +0200

    integration test for error

--- src/test/java/com/tui/fly/IntegrationTest.java
+++ src/test/java/com/tui/fly/IntegrationTest.java
@@ -1,5 +1,6 @@
 package com.tui.fly;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,10 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.NoSuchElementException;
+
 import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,7 +32,15 @@ public class IntegrationTest extends AbstractConfigTest {
             .andExpect(status().isOk())
             .andExpect(jsonPath("$..iataCode", hasItems("FRA", "MUC")));
     }
-    
+
+    @Test
+    public void unknownAirportIsHandledWithState() throws Exception {
+        mvc.perform(get("/rest/airport/XYZ"))
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$exception", is(NoSuchElementException.class.getName())))
+            .andExpect(jsonPath("$message", Matchers.containsString("XYZ")));
+    }
+
     @Before
     public void createMvc() {
         mvc = webAppContextSetup(context).build();
