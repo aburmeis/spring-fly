d76c97f436337fdf800d61c0eaed1e54dedfc9ee
Arne Burmeister <arne.burmeister@tui.com>
2015-10-12 13:09:36 +0200

    integration test for airports

--- src/test/java/com/tui/fly/IntegrationTest.java
+++ src/test/java/com/tui/fly/IntegrationTest.java
@@ -1,12 +1,22 @@
 package com.tui.fly;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @ActiveProfiles("database")
 @TestPropertySource({"classpath:application.properties", "classpath:application-database.properties"})
@@ -16,8 +26,23 @@ public class IntegrationTest extends AbstractConfigTest {
     private WebApplicationContext context;
     private MockMvc mvc;
 
+    @Test
+    public void germanAirportsAreFound() throws Exception {
+        mvc.perform(get("/rest/airport/search").param("country", "DE"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$..iataCode", hasItems("FRA", "MUC")));
+    }
+
+    @Test
+    public void unknownAirportIsHandledWithState() throws Exception {
+        mvc.perform(get("/rest/airport/XYZ"))
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$exception", is(NoSuchElementException.class.getName())))
+            .andExpect(jsonPath("$message", Matchers.containsString("XYZ")));
+    }
+
     @Before
     public void createMvc() {
-        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+        mvc = webAppContextSetup(context).build();
     }
 }
