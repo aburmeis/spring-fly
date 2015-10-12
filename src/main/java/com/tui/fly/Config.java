97bb6b4ec00d526f5a81a06c5a19895e18375ca2
Arne Burmeister <arne.burmeister@tui.com>
2015-10-12 11:41:16 +0200

    simplified config

--- src/main/java/com/tui/fly/Config.java
+++ src/main/java/com/tui/fly/Config.java
@@ -1,21 +1,17 @@
 package com.tui.fly;
 
-import com.tui.fly.domain.ConnectionToStringConverter;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
-class Config extends WebMvcConfigurerAdapter {
+class Config {
 
     @Bean
     @Profile("memory")
@@ -27,10 +23,5 @@ class Config extends WebMvcConfigurerAdapter {
     @Profile("memory")
     public Resource airportData() {
         return new ClassPathResource("airports.csv");
-    }
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new ConnectionToStringConverter());
     }
 }
