ae2f78262cc7a06028d7a14b5c069ecb288d15f5
Arne Burmeister <arne.burmeister@tui.com>
2015-10-14 13:36:28 +0200

    added google maps

--- src/main/java/com/tui/fly/web/FormController.java
+++ src/main/java/com/tui/fly/web/FormController.java
@@ -4,9 +4,12 @@ import com.tui.fly.domain.Airport;
 import com.tui.fly.domain.Country;
 import com.tui.fly.service.AirportRegistry;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,8 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -31,6 +36,12 @@ class FormController {
 
     @Autowired
     private AirportRegistry airports;
+
+    @Autowired
+    private ObjectMapper json;
+    
+    @Value("${google.maps.api.key}")
+    String googleMapsApiKey;
 
     @ModelAttribute("countries")
     public List<Country> populateCountries() {
@@ -54,8 +65,32 @@ class FormController {
         } else {
             found = airports.findAirports();
         }
-        model.addAttribute("airports", found);
+        model.addAttribute("airports", found).addAttribute("googleMapsApiKey", googleMapsApiKey).addAttribute("mapData", asJson(createAllMapData(found)));
         LOG.debug("airport model {}", model);
         return new ModelAndView("airport", model);
+    }
+
+    private List<Map<String, Object>> createAllMapData(Set<Airport> found) {
+        return found.stream().filter(ap -> ap.getLocation() != null).map(this::createMapData).collect(toList());
+    }
+
+    private Map<String,Object> createMapData(Airport airport) {
+        Map<String, Object> data = new HashMap<>();
+        Map<String, Double> position = new HashMap<>();
+        position.put("lat", airport.getLocation().getLatitude());
+        position.put("lng", airport.getLocation().getLongitude());
+        data.put("position", position);
+        data.put("label", airport.getIataCode().charAt(0));
+        data.put("title", airport.getName() + " (" + airport.getIataCode() + ')');
+        return data;
+    }
+
+    private String asJson(Object data) {
+        try {
+            return json.writeValueAsString(data);
+        } catch (JsonProcessingException e) {
+            LOG.error("Cannot create JSON from {}", data, e);
+            return "";
+        }
     }
 }
--- src/test/java/com/tui/fly/web/FormControllerTest.java
+++ src/test/java/com/tui/fly/web/FormControllerTest.java
@@ -2,11 +2,15 @@ package com.tui.fly.web;
 
 import com.tui.fly.domain.Airport;
 import com.tui.fly.domain.Country;
+import com.tui.fly.domain.Location;
 import com.tui.fly.domain.String2CountryConverter;
 import com.tui.fly.service.AirportRegistry;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -14,14 +18,25 @@ import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
 import static com.tui.fly.domain.Airport.airport;
 import static com.tui.fly.domain.Country.country;
+import static java.util.Arrays.asList;
 import static java.util.Collections.singleton;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -35,6 +50,8 @@ public class FormControllerTest {
     private FormController controller;
     @Mock
     private AirportRegistry airports;
+    @Mock
+    private ObjectMapper json;
 
     private MockMvc mvc;
 
@@ -45,7 +62,7 @@ public class FormControllerTest {
             .andExpect(view().name("index"))
             .andExpect(model().attribute("countries", hasItems(country("DE"), country("GB"), country("US"))))
             .andExpect(model().attribute("form", instanceOf(AirportSearch.class)))
-            .andExpect(model().attributeDoesNotExist("airports"));
+            .andExpect(model().attributeDoesNotExist("airports", "googleMapsApiKey", "mapData"));
     }
 
     @Test
@@ -57,8 +74,31 @@ public class FormControllerTest {
         mvc.perform(post("/airport.html"))
             .andExpect(status().isOk())
             .andExpect(view().name("airport"))
-            .andExpect(model().attributeExists("countries", "form"))
+            .andExpect(model().attributeExists("countries", "form", "googleMapsApiKey", "mapData"))
             .andExpect(model().attribute("airports", hasItem(FRA)));
+    }
+
+    @Test
+    public void onlyAirportsWithLocationArePutOnMap() throws Exception {
+        Airport FRA = airport("FRA");
+        FRA.setName("Frankfurt");
+        FRA.setLocation(new Location(50.026421, 8.543125));
+        Airport BER = airport("BER");
+        when(airports.findAirports())
+            .thenReturn(new HashSet<>(asList(FRA)));
+
+        mvc.perform(post("/airport.html"))
+            .andExpect(status().isOk());
+
+        ArgumentCaptor<Object> mapData = ArgumentCaptor.forClass(Object.class);
+        verify(json).writeValueAsString(mapData.capture());
+        @SuppressWarnings("unchecked")
+        List<Map<String, Object>> allMapData = (List<Map<String, Object>>) mapData.getValue();
+        assertThat(allMapData.size(), is(1));
+        Map<String, Object> fraMapData = allMapData.get(0);
+        assertThat(fraMapData.get("label"), is('F'));
+        assertThat(fraMapData.get("title"), is("Frankfurt (FRA)"));
+        assertThat((Map<String, Double>) fraMapData.get("position"), allOf(hasEntry("lat", 50.026421), hasEntry("lng", 8.543125)));
     }
 
     @Test
@@ -70,7 +110,7 @@ public class FormControllerTest {
         mvc.perform(post("/airport.html").param("iataCode", "FRA"))
             .andExpect(status().isOk())
             .andExpect(view().name("airport"))
-            .andExpect(model().attributeExists("countries", "form"))
+            .andExpect(model().attributeExists("countries", "form", "googleMapsApiKey", "mapData"))
             .andExpect(model().attribute("airports", hasItem(FRA)));
     }
 
@@ -83,15 +123,19 @@ public class FormControllerTest {
         mvc.perform(post("/airport.html").param("country", "DE"))
             .andExpect(status().isOk())
             .andExpect(view().name("airport"))
-            .andExpect(model().attributeExists("countries", "form"))
+            .andExpect(model().attributeExists("countries", "form", "googleMapsApiKey", "mapData"))
             .andExpect(model().attribute("airports", hasItem(FRA)));
     }
 
     @Before
-    public void createMvc() {
+    public void createMvc() throws JsonProcessingException {
         MockitoAnnotations.initMocks(this);
         DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
         conversionService.addConverter(new String2CountryConverter());
+        controller.googleMapsApiKey = "ABCD";
         mvc = MockMvcBuilders.standaloneSetup(controller).setConversionService(conversionService).build();
+        
+        when(json.writeValueAsString(any(Collection.class)))
+            .thenReturn("[]");
     }
 }
